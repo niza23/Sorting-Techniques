6db027853b96c9e3a6ecbebab8592bda27aeeb16
Nidhi Zala <nidhizala2307@gmail.com>
2022-06-16 16:57:22 +0530

    Add files via upload

---
+++ InsertionSort.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+void insertionSort(int a[], int n)
+{
+    
+    for(int i=1;i<n;i++)  //i=1 
+    {
+        int j=i-1,x;
+        x=a[i];
+        while(j>-1 && a[j]>x) //shifting
+        {
+            a[j+1]=a[j];
+            j--;
+        }
+        a[j+1]=x;
+    }
+}
+int main()
+{
+    int a[]={3,7,9,10,5,6,5};
+    int n=7;
+    insertionSort(a,n);
+
+    for(int i=0; i<7; i++)
+    {
+        cout<<a[i]<<endl;
+    }
+
+    return 0;
+}
